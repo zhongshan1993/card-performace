a8c0d2a0971416a621c1070e8c4852fc3776745c
钟山 <940817@nd.com>
2018-10-27 16:42:37 +0800

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import store from "./store";
+import user from "./images/user.png";
+import "./App.css";
+import { setProfession } from "./actions";
 
 class App extends Component {
+  handleProfessionUpdate = () => {
+    //a list of professions
+    const professions = [
+      "Dancer",
+      "Fashion Designer",
+      "Bartender",
+      "Wrestler",
+      "Librarian",
+      "Singer",
+      "Coder",
+      "Teacher"
+    ];
+    //pick a random profession from the list above
+    const randomProfession =
+      professions[Math.floor(Math.random() * professions.length)];
+    //dispatch action creator with random professions
+    store.dispatch(setProfession(randomProfession));
+  };
+
   render() {
+    const { name, description, likes, location } = store.getState();
+    store.speak();
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <section className="User__img">
+          <img src={user} alt="user" />
+        </section>
+
+        <section className="User__info">
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {" "}
+            <span className="faint">I am</span> a {description}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <p>
+            {" "}
+            <span className="faint">I like</span> {likes}
+          </p>
+
+          <p className="User__info__details User__info__divider faint">
+            <span>Name: </span>
+            <span>{name}</span>
+          </p>
+          <p className="User__info__details faint">
+            <span>Location: </span>
+            <span>{location}</span>
+          </p>
+        </section>
+
+        <div className="App__update">
+          <button onClick={this.handleProfessionUpdate}>
+            Change user's profession
+          </button>
+        </div>
       </div>
     );
   }
